5e4060962f20b70075cd815385921ac903064d59
Vadim Havryluk <gawad@ukr.net>
2024-04-26 21:28:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
-const TOKEN = "6087241117:AAE9o2D2mmAvuhNioU4cpj-WQvjMFWT4Gkg";
+const TOKEN = "";
 const server = express();
 const bot = new TelegramBot(TOKEN, {
     polling: true
